7ecf2983ada85ae56295e235010c077a8b71ce7f
cmutd <16281302+cmutd@users.noreply.github.com>
2020-10-29 17:53:35 -0500

    Update code.R

--- code.R
+++ code.R
@@ -1,9 +1,11 @@
 library(rvest)
 
-request <- c("MIRT000019","MIRT001950")
+request <- c("MIRT302435")
 
 seq <- function(mima){
   url <- paste0("http://mirtarbase.mbc.nctu.edu.tw/php/detail.php?mirtid=", mima,"#target")
+  
+  print(paste("processing ",mima))
   
   a <- htmltools::HTML(url)
   predict <- read_html(a)%>% xml_find_all('//*[@style="background-color:yellow"]')%>%html_text()%>%
@@ -11,9 +13,11 @@ seq <- function(mima){
   confirm <- read_html(a)%>% xml_find_all('//*[@style="background-color:blue"]')%>%html_text()%>%
     gsub("[^A-Za-z]","",.)
   
+  predict <- ifelse(length(predict)==0,"NA",predict)
   confirm <- ifelse(length(confirm)==0,"NA",confirm)
   tmp <- data.frame(predict=predict,confirm=confirm,mir=mima)
-  print(paste0(mima," is done"))
+  
+
 }
 
 test <- lapply(request,seq)
