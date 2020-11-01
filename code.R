d4ef29b012be3ef43bb8f0020fb950d152b7559f
cmutd <16281302+cmutd@users.noreply.github.com>
2020-11-01 15:31:04 -0600

    Update code.R

--- code.R
+++ code.R
@@ -1,19 +1,37 @@
+library(rvest)
+library(stringr)
+library(data.table)
+
+request <- c("MIRT003240","MIRT004055","MIRT006216")
+
 seq <- function(mima){
   url <- paste0("http://mirtarbase.mbc.nctu.edu.tw/php/detail.php?mirtid=", mima,"#target")
   
   print(paste("processing ",mima))
   
   a <- htmltools::HTML(url)
-  predict <- read_html(a)%>% xml_find_all('//*[@style="background-color:yellow"]')%>%html_text()%>%
-    gsub("[^A-Za-z]","",.)
+  # predict <- read_html(a)%>% xml_find_all('//*[@style="background-color:yellow"]')%>%html_text()%>%
+  #   gsub("[^A-Za-z]","",.)
   confirm <- read_html(a)%>% xml_find_all('//*[@style="background-color:blue"]')%>%html_text()%>%
     gsub("[^A-Za-z]","",.)
   
-  predict <- ifelse(length(predict)==0,"NA",predict)
-  confirm <- ifelse(length(confirm)==0,"NA",confirm)
-  tmp <- data.frame(predict=predict,confirm=confirm,mir=mima)
+  refid <- read_html(a)%>% xml_find_all('//*[@id="blueDream"]/table/tbody/*/td/font/pre')%>%html_text()%>%str_split("\\|")%>%
+    .[[1]]%>%.[2]
+  
+  symbol <- read_html(a)%>% xml_find_all('//*[@id="blueDream"]/table/tbody/*/td/font/pre')%>%html_text()%>%str_split("\\|")%>%.[[1]]%>%
+    .[1]%>%gsub(">","",.)
+  
+  if (length(confirm)>0){
+    result <- data.frame(confirm,mima,refid,symbol)
+  }else{
+    result <- data.frame(confirm="NA",mima,refid,symbol)
+  }
+  
+  return(result)
   
   
 }
 
 test <- lapply(request,seq)
+result <- do.call(rbind,test)
+write.table(result,"confirm.txt",row.names = F,quote=F)
